1c5a127992990dc7902852e9c3e03f9dd1b8ae40
Andrew Ibsen <andrewibsen@gmail.com>
2026-03-03 18:37:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -50,7 +50,8 @@ function initMapMarkers() {
     { name: 'London (LHR)', description: 'Base', x: 0.45, y: 0.30 },
     { name: 'Reykjavik (KEF)', description: 'Base', x: 0.42, y: 0.25 },
     { name: 'Geneva (GVA)', description: 'Base', x: 0.47, y: 0.33 },
-    { name: 'Florida', description: 'Base', x: 0.30, y: 0.48 }
+    { name: 'Florida (MCO)', description: 'Base', x: 0.30, y: 0.48 }
+    { name: 'Rio de Janeiro (GIG)', description: 'Base', x: 0.30, y: 0.22 }
   ];
 
   function renderMarkers() {
